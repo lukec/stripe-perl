aae5aed5f27443a2c0fcd0cd7e0586455e4f1603
Rusty Conover <rusty@luckydinosaur.com>
2014-03-15 00:07:45 -0400

    Add support for statement_description in plans.

--- lib/Net/Stripe/Plan.pm
+++ lib/Net/Stripe/Plan.pm
@@ -6,19 +6,25 @@ extends 'Net::Stripe::Resource';
 
 union 'StripePlan', ['Str', 'Net::Stripe::Plan'];
 
+subtype 'StatementDescription',
+    as 'Str',
+    where { $_ =~ /^[^<>"']{0,15}$/ },
+    message { "The statement description you provided '$_' must be 15 characters or less and not contain <>\"'." };
+
 has 'id'                => (is => 'ro', isa => 'Maybe[Str]', required => 1);
 has 'amount'            => (is => 'ro', isa => 'Maybe[Int]', required => 1);
 has 'currency'          => (is => 'ro', isa => 'Maybe[Str]', required => 1);
-has 'interval_count'    => (is => 'ro', isa => 'Maybe[Int]', required => 0);
 has 'interval'          => (is => 'ro', isa => 'Maybe[Str]', required => 1);
+has 'interval_count'    => (is => 'ro', isa => 'Maybe[Int]', required => 0);
 has 'name'              => (is => 'ro', isa => 'Maybe[Str]', required => 1);
 has 'trial_period_days' => (is => 'ro', isa => 'Maybe[Int]');
+has 'statement_description' => ('is' => 'ro', isa => 'Maybe[StatementDescription]', required => 0);
 
 method form_fields {
     return (
         map { $_ => $self->$_ }
             grep { defined $self->$_ }
-                qw/id amount currency interval interval_count name trial_period_days/
+                qw/id amount currency interval interval_count name statement_description trial_period_days/
     );
 }
 
