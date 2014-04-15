55c9f445185bc4b663d6c03b0d1775adce9b9a5d
Devin M. Ceartas <devin@nacredata.com>
2014-04-15 17:43:47 -0400

    Note removal of "count" attribute in recent Stripe update to Lists

--- lib/Net/Stripe/SubscriptionList.pm
+++ lib/Net/Stripe/SubscriptionList.pm
@@ -4,7 +4,7 @@ use methods;
 extends 'Net::Stripe::Resource';
 
 has 'object' => (is => 'ro', isa => 'Str');
-has 'count'  => (is => 'ro', isa => 'Int');
+has 'count'  => (is => 'ro', isa => 'Int'); # no longer included by default, see note below
 has 'url'    => (is => 'ro', isa => 'Str');
 has 'data'   => (is => 'ro', isa => 'ArrayRef[Net::Stripe::Subscription]');
 
@@ -14,3 +14,9 @@ method form_fields {
 
 __PACKAGE__->meta->make_immutable;
 1;
+
+__END__
+From the Stripe Change Log:
+2014-03-28
+Remove count property from list responses, replacing it with the optional property total_count. 
+You can request that total_count be included in your responses by specifying include[]=total_count.
