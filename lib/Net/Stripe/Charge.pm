e23e0b61b9758bafd1481dfd0d646835679f5222
Sherrard Burton <sburton@allafrica.com>
2018-07-14 13:06:20 +0000

    oversight that causes capture to default to false

--- lib/Net/Stripe/Charge.pm
+++ lib/Net/Stripe/Charge.pm
@@ -28,10 +28,11 @@ has 'receipt_email'       => (is => 'ro', isa => 'Maybe[Str]');
 has 'capture'             => (is => 'ro', isa => 'Maybe[Bool]');
 
 method form_fields {
+    my $capture = ( !defined( $self->capture ) || $self->capture ) ? 'true' : 'false';
     return (
         $self->fields_for('card'),
         $self->form_fields_for_metadata(),
-        capture => $self->capture ? 'true' : 'false',
+        capture => $capture,
         map { $_ => $self->$_ }
             grep { defined $self->$_ }
                 qw/amount currency customer description application_fee receipt_email/
