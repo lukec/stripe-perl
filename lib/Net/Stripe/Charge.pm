29fec4341f75e5aa8b866c9ea49bed7f964bec29
Andrew Solomon <andrew@geekuni.com>
2019-12-01 14:07:25 +0000

    Centralise boolean post parameter generation - Sherrard Burton

--- lib/Net/Stripe/Charge.pm
+++ lib/Net/Stripe/Charge.pm
@@ -26,17 +26,15 @@ has 'metadata'            => (is => 'rw', isa => 'Maybe[HashRef]');
 has 'invoice'             => (is => 'ro', isa => 'Maybe[Str]');
 has 'receipt_email'       => (is => 'ro', isa => 'Maybe[Str]');
 has 'status'              => (is => 'ro', isa => 'Maybe[Str]');
-has 'capture'             => (is => 'ro', isa => 'Maybe[Bool]');
+has 'capture'             => (is => 'ro', isa => 'Bool', default=> 1);
 
 method form_fields {
-    my $capture = ( !defined( $self->capture ) || $self->capture ) ? 'true' : 'false';
     return (
         $self->fields_for('card'),
         $self->form_fields_for_metadata(),
-        capture => $capture,
-        map { $_ => $self->$_ }
+        map { $_ => $self->get_form_field_value( $_ ) }
             grep { defined $self->$_ }
-                qw/amount currency customer description application_fee receipt_email/
+                qw/amount currency customer description application_fee receipt_email capture/
     );
 }
 
