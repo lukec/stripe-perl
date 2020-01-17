71c4e9fa15c73f2f4dbe48e125c728e16f477612
Sherrard Burton <32931314+sherrardb@users.noreply.github.com>
2020-01-17 02:40:41 +0000

    update statement_description to statement_descriptor:  * add failing test to detect ignored attribute `statement_descriptor` in Net::Stripe::Charge  * add missing `statement_descriptor` attribute in Net::Stripe::Charge  * update `statement_description` to `statement_descriptor` Net::Stripe::Plan  * API docs <https://stripe.com/docs/upgrades#2014-12-17> indicate that this change is backwards-compatible

--- lib/Net/Stripe/Plan.pm
+++ lib/Net/Stripe/Plan.pm
@@ -7,10 +7,10 @@ extends 'Net::Stripe::Resource';
 
 # ABSTRACT: represent a Plan object from Stripe
 
-subtype 'StatementDescription',
+subtype 'StatementDescriptor',
     as 'Str',
     where { !defined($_) || $_ =~ /^[^<>"']{0,15}$/ },
-    message { "The statement description you provided '$_' must be 15 characters or less and not contain <>\"'." };
+    message { "The statement descriptor you provided '$_' must be 15 characters or less and not contain <>\"'." };
 
 has 'id'                => (is => 'ro', isa => 'Maybe[Str]', required => 1);
 has 'amount'            => (is => 'ro', isa => 'Maybe[Int]', required => 1);
@@ -19,13 +19,13 @@ has 'interval'          => (is => 'ro', isa => 'Maybe[Str]', required => 1);
 has 'interval_count'    => (is => 'ro', isa => 'Maybe[Int]', required => 0);
 has 'name'              => (is => 'ro', isa => 'Maybe[Str]', required => 1);
 has 'trial_period_days' => (is => 'ro', isa => 'Maybe[Int]');
-has 'statement_description' => ('is' => 'ro', isa => 'Maybe[StatementDescription]', required => 0);
+has 'statement_descriptor' => (is => 'ro', isa => 'Maybe[StatementDescriptor]', required => 0);
 
 method form_fields {
     return (
         map { $_ => $self->$_ }
             grep { defined $self->$_ }
-                qw/id amount currency interval interval_count name statement_description trial_period_days/
+                qw/id amount currency interval interval_count name statement_descriptor trial_period_days/
     );
 }
 
