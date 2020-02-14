cb271ac345633e48bf30266fff82e655a9955af2
sherrardb <32931314+sherrardb@users.noreply.github.com>
2020-02-14 03:43:27 +0000

    add metadata for Coupon  * clean up and augment unit tests  * update POD

--- lib/Net/Stripe/Coupon.pm
+++ lib/Net/Stripe/Coupon.pm
@@ -12,10 +12,12 @@ has 'duration'           => (is => 'rw', isa => 'Maybe[Str]', required => 1);
 has 'duration_in_months' => (is => 'rw', isa => 'Maybe[Int]');
 has 'max_redemptions'    => (is => 'rw', isa => 'Maybe[Int]');
 has 'redeem_by'          => (is => 'rw', isa => 'Maybe[Int]');
+has 'metadata'           => (is => 'ro', isa => 'Maybe[HashRef]');
 
 method form_fields {
     return $self->form_fields_for(
-        qw/id percent_off duration duration_in_months max_redemptions redeem_by/
+        qw/id percent_off duration duration_in_months max_redemptions redeem_by
+            metadata/
     );
 }
 
